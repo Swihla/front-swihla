47d5acbce1cb2284f7371877a450a83158b717c7
lhoussaine bouaik <bouaik.lhou@gmail.com>
2021-06-01 11:13:42 +0100

    Consume api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from 'react'
 
 function App() {
+  const [message, setMessage] = useState("Api call working not working")
+  useEffect(() => {
+    fetch('http://localhost:3040/home')
+      .then(res => res.json())
+      .then(res => setMessage(res.message))
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{message}</h1>
     </div>
   );
 }
